1c0af2681bc582fa2cf594dc21c6ec8ffa66aa93
zrdouane <ramirdouane@gmail.com>
2024-01-11 18:58:47 +0000

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+takes a float a and a float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function adds two numbers and returns the result.
+    """
+    return a + b
---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/zrdouane/alx-backend-python.git
