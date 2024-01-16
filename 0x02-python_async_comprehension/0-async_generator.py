718ce37b084efae7cac6547e2c9b5d5e3cc4948a
Call me zack <85793706+zrdouane@users.noreply.github.com>
2024-01-16 21:19:49 +0000

    add

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-'''Asynchronous Python
-'''
-import random
+""" Module documentation """
 import asyncio
+import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    '''yeild a random number at 1sec interval
-    '''
+    """Func doc"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
